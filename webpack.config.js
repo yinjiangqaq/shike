12ed41aed8d9ccf1c35982ce4d66eb4faf272806
yinjiangqaq <1139277782@qq.com>
2019-09-08 10:48:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,97 @@
 const path = require('path');
 var htmlWebpackPlugin = require("html-webpack-plugin");
-const VueLoaderPlugin=require('vue-loader/lib/plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require("webpack");
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CompressPlugin = require('compression-webpack-plugin');
 module.exports = {
-  entry: './src/main.js',
-     devServer: {
-         contentBase: './dist'
-       },
-    
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new htmlWebpackPlugin({
-      template: path.join(__dirname, "./src/index.html"),
-      filename: "index.html"
-    }),
-    new VueLoaderPlugin(),
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery'
-  })
-  ],
-  module: {
-         rules: [
-           {
-            test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-           },
-           {
-             test:/\.less$/,
-             use:[
-              'style-loader',
-              'css-loader',
-              'less-loader'
-             ]
-           },
-           {
-            test:/\.scss$/,
-            use:[
-             'style-loader',
-             'css-loader',
-             'sass-loader'
-            ]
-           },
-           {
-             test:/\.(png|svg|jpg|gif)$/,
-             use:[{
-               loader:'url-loader',
-               options:{
-                 limit:10000,
-                 name: path.posix.join('static', 'img/[name].[hash:7].[ext]')
-               }
-             }]
-           },
-           {
-             test:/\.(ttf|eot|svg|woff|woff2)$/,
-             use:'url-loader'
-           },
-           {
-             test:/\.js$/,use:'babel-loader',exclude:/node_modules/
-           },
-           {
-             test:/\.vue$/,use:'vue-loader'
-           },
-           {
-            test: require.resolve('jquery'),
-            use: [{
-              loader: 'expose-loader',
-              options: '$'
-            }]
-          }
-         ]
-       }
-    
+    entry: './src/main.js',
+    // devServer: {
+    //     contentBase: './dist'
+    // },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, "./src/index.html"),
+            filename: "index.html"
+        }),
+        new VueLoaderPlugin(),
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery'
+        }),
+        // new CompressPlugin({
+        //     filename: '[path].gz[query]',
+        //     algorithm: 'gzip',
+        //     test: /\.js$|\.css|\.html$/,
+        // }),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                compress: false
+            }
+        })
+    ],
+
+
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: path.posix.join('static', 'img/[name].[hash:7].[ext]')
+                    }
+                }]
+            },
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)$/,
+                use: 'url-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            }
+        ]
+    },
+
 };
